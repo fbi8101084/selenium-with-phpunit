3d4075878d8c23c2d5dc5b8c9352a889e7c934ca
nobita <unvisityboss@gmail.com>
2015-04-27 17:48:29 +0800

    add _assertAdElementPresent

--- CustomSelenium2TestCase.php
+++ CustomSelenium2TestCase.php
@@ -23,4 +23,29 @@ class CustomSelenium2TestCase extends PHPUnit_Extensions_Selenium2TestCase {
         file_get_contents($url);
         return (bool) preg_match('/.+' . $status . '.+/i', $http_response_header[0]);
     }
+
+    protected function _assertAdElementPresent($cssSelector) {
+        try {
+            // 檢查外框是否存在
+            $adBlocks = $this->elements($this->using('css selector')->value($cssSelector));
+
+            foreach ($adBlocks as $adBlock) {
+                $this->assertEquals(true, $adBlock->displayed(), 'Test Failed: ' . $cssSelector . ' 廣告不可見（工程部協助）。\n');
+                if ($adBlock->displayed()) {
+
+                    // 檢查是否有廣告內容
+                    $items = $this->elements($this->using('css selector')->value($cssSelector . '>div'));
+
+                    foreach ($items as $item) {
+                        $this->assertEquals(true, $item->displayed(), 'Test Failed: ' . $cssSelector . ' 的內容不可見（）。\n');
+                    }
+                }
+            }
+            return true;
+
+        } catch (Exception $e) {
+            echo $e->getMessage() . "\n";
+            return false;
+        }
+    }
 }
