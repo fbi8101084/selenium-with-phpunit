b4ddeb6e87da3bbee221f6be8d1562278207ff13
nobita <unvisityboss@gmail.com>
2015-04-27 17:46:37 +0800

    update browser list

--- CustomSelenium2TestCase.php
+++ CustomSelenium2TestCase.php
@@ -1,25 +1,24 @@
 <?php
 class CustomSelenium2TestCase extends PHPUnit_Extensions_Selenium2TestCase {
-    public static function browsers() {
-        return array(
-//            array(
-//                'name'    => 'Firefox on Mac',
-//                'browser' => 'firefox',
-//                'browserName' => 'firefox',
-//                'host'    => '0.0.0.0',
-//                'port'    => 4444,
-//                'timeout' => 30000,
-//            ),
-            array(
-                'name'    => 'Chrome on Mac',
-                'browser' => 'chrome',
-                'browserName' => 'chrome',
-                'host'    => '0.0.0.0',
-                'port'    => 4444,
-                'timeout' => 30000,
-            )
-        );
-    }
+    protected static $_browsersList = array(
+        'firefox' => array(
+            'name'    => 'Firefox on Mac',
+            'browser' => 'firefox',
+            'browserName' => 'firefox',
+            'host'    => '0.0.0.0',
+            'port'    => 4444,
+            'timeout' => 30000,
+        ),
+        'chrome' => array(
+            'name'    => 'Chrome on Mac',
+            'browser' => 'chrome',
+            'browserName' => 'chrome',
+            'host'    => '0.0.0.0',
+            'port'    => 4444,
+            'timeout' => 30000,
+        )
+    );
+
     protected function _assertRequestStatus($url, $status = 200) {
         file_get_contents($url);
         return (bool) preg_match('/.+' . $status . '.+/i', $http_response_header[0]);
