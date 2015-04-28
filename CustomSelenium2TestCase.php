5ec0f1eb008e388bfa83b69f77b437f7d4fe08ee
nobita <unvisityboss@gmail.com>
2015-04-28 14:52:25 +0800

    restyle

--- CustomSelenium2TestCase.php
+++ CustomSelenium2TestCase.php
@@ -2,20 +2,16 @@
 class CustomSelenium2TestCase extends PHPUnit_Extensions_Selenium2TestCase {
     protected static $_browsersList = array(
         'firefox' => array(
-            'name'    => 'Firefox on Mac',
-            'browser' => 'firefox',
             'browserName' => 'firefox',
             'host'    => '0.0.0.0',
             'port'    => 4444,
-            'timeout' => 30000,
+            'timeout' => 30000
         ),
         'chrome' => array(
-            'name'    => 'Chrome on Mac',
-            'browser' => 'chrome',
             'browserName' => 'chrome',
             'host'    => '0.0.0.0',
             'port'    => 4444,
-            'timeout' => 30000,
+            'timeout' => 30000
         )
     );
 
