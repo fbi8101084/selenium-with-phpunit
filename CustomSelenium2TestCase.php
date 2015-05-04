7dfbd13cc848979282c9a8aa487ce916791b7c40
nobita <unvisityboss@gmail.com>
2015-05-04 16:40:47 +0800

    他不能是 static 會噴錯誤

--- CustomSelenium2TestCase.php
+++ CustomSelenium2TestCase.php
@@ -16,7 +16,7 @@ abstract class CustomSelenium2TestCase extends PHPUnit_Extensions_Selenium2TestC
         )
     );
 
-    public static function browsers()
+    public function browsers()
     {
         return array(
             self::$_browsersList['firefox']
