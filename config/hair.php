390fd9cdaa36879605cf8b7c11fc0faafb89bd8b
John xie <fg@JohnAir.lan>
2015-05-05 17:47:53 +0800

    新增hair config

---
+++ config/hair.php
@@ -0,0 +1,19 @@
+<?
+return array(
+    '/' => array(
+        'imgs' => array(
+            '//*[@id="promo"]/div/div/ul/li[1]/div[3]/a/img' => '圖片左',
+            '//*[@id="promo"]/div/div/ul/li[3]/div[2]/a/img' => '圖片中 左上',
+            '//*[@id="promo"]/div/div/ul/li[5]/div[2]/a/img' => '圖片中 左下',
+            '//*[@id="promo"]/div/div/ul/li[4]/div[2]/a/img' => '圖片中 右上',
+            '//*[@id="promo"]/div/div/ul/li[6]/div[2]/a/img' => '圖片中 右下',
+            '//*[@id="promo"]/div/div/ul/li[2]/div[5]/a/img' => '圖片右'
+        ),
+
+        'ad' => array(
+            'AdBlock AdBlock_Loc_153',
+            'AdBlock AdBlock_Loc_156',
+            'AdBlock AdBlock_Loc_155'
+        )
+    ),
+);
